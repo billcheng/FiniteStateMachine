e3bda1fa539f2afa1e5e00a29d1eea5ddeab49de
billcheng <cccheng118@yahoo.com>
2015-11-12 10:12:07 -0600

    Init version

---
+++ Example/ChopperAI.cs
@@ -0,0 +1,216 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChopperAI : MonoBehaviour
+{
+
+    StateMachine sm = new StateMachine("main");
+    StateMachine subsm = new StateMachine("helicopter");
+    public float Height = 50;
+    public float AscendingSpeed = 1;
+    public float DescendingSpeed = 1;
+    //public float MomentumSpeed = 1;
+    public float MoveSpeed = 1;
+    public float TurningSpeed = 1;
+    public Vector3[] PatrolCheckPoints;
+    private Vector3 _moveTo;
+    int _patrolIndex;
+    public bool Verbose = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        SetupMain();
+        SetupSub();
+        sm.Verbose = subsm.Verbose = Verbose;
+    }
+
+    private void SetupMain()
+    {
+        float time = 0;
+
+        sm
+            .State("idle")
+                .Reset()
+                .OnEntry(() => time = Time.time)
+                .Condition(() => Time.time - time >= 3, "patrol")
+            .State("patrol")
+                .OnEntry(() =>
+                {
+                    // find the nearest patrol check point
+                    subsm.Reset("decide");
+                    _patrolIndex = PatrolCheckPoints.FindMinIndex(p => Vector3.Distance(p, transform.position));
+                    _moveTo = _patrolIndex != -1 ? PatrolCheckPoints[_patrolIndex] : transform.position;
+                })
+                .OnState(() => subsm.Update())
+            .State("attack")
+            .State("retreat")
+            .State("heal")
+            ;
+    }
+
+    private void SetupSub()
+    {
+        var originalPos = Vector3.zero;
+        float speed = 0;
+        float percent = 0;
+        Vector3 tempTarget = Vector3.zero;
+
+        subsm
+            .State("decide")
+                .Condition(() => transform.position.y - _moveTo.y > 50, "descending")
+                .Condition(() => transform.position.y - _moveTo.y < -50, "ascending")
+                .Condition(() => true, "alignAngle")
+            .State("ascending")
+                .OnEntry(() =>
+                {
+                    originalPos = transform.position;
+                    tempTarget = new Vector3(transform.position.x, _moveTo.y, transform.position.z);
+                    percent = 0;
+                })
+                .OnState(() =>
+                {
+                    speed = percent.HillSlope(1, 0.5f, 4);
+                    percent = Mathf.Min(percent + AscendingSpeed * Time.deltaTime, 2f);
+
+                    transform.position = Vector3.Lerp(originalPos, tempTarget, speed);
+                })
+                .Condition(() => Vector3.Distance(transform.position, tempTarget) <= 5, "moveTo")
+            .State("descending")
+                .Reset()
+                .OnEntry(() =>
+                {
+                    originalPos = transform.position;
+                    percent = 0;
+
+                    RaycastHit hitInfo;
+                    if (Physics.Raycast(transform.position, Vector3.down, out hitInfo))
+                        tempTarget = new Vector3(transform.position.x, hitInfo.point.y, transform.position.z);
+                })
+                .OnState(() =>
+                {
+                    speed = percent.HillSlope(1, 0.5f, 4);
+                    percent = Mathf.Min(percent + DescendingSpeed * Time.deltaTime, 2f);
+                    transform.position = Vector3.Lerp(originalPos, tempTarget, speed);
+                })
+                .Condition(() => Vector3.Distance(transform.position, tempTarget) <= 1, "moveTo")
+            .State("alignAngle")
+                .OnEntry(() =>
+                {
+                })
+                .OnState(() =>
+                {
+                    var dir = _moveTo - transform.position;
+                    dir.y = 0;
+                    var newRot = Quaternion.LookRotation(dir);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, newRot, TurningSpeed * Time.deltaTime);
+
+                    transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+
+                    if (transform.position.y < _moveTo.y-0.5)
+                        transform.Translate(Vector3.up * MoveSpeed * Time.deltaTime);
+                    if (transform.position.y > _moveTo.y + 0.5)
+                        transform.Translate(Vector3.down * MoveSpeed * Time.deltaTime);
+
+                })
+                .Condition(() =>
+                {
+                    var dir = _moveTo - transform.position;
+                    dir.y = 0;
+                    var angle = Vector3.Angle(transform.forward, dir);
+                    if (angle < 15)
+                        return true;
+
+                    if (Vector3.Distance(_moveTo, transform.position) <= 1)
+                        return true;
+
+                    return false;
+                }, "moveTo")
+            .State("moveTo")
+                .OnEntry(() =>
+                {
+                    originalPos = transform.position;
+                    percent = 0;
+                })
+                .OnState(() =>
+                {
+                    var dir = (_moveTo - transform.position).normalized;
+
+                    dir.y = 0;
+                    var newRot = Quaternion.LookRotation(dir);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, newRot, TurningSpeed * Time.deltaTime);
+
+                    transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+
+                    if (transform.position.y < _moveTo.y - 0.5)
+                        transform.Translate(Vector3.up * MoveSpeed * Time.deltaTime);
+                    if (transform.position.y > _moveTo.y + 0.5)
+                        transform.Translate(Vector3.down * MoveSpeed * Time.deltaTime);
+                })
+                .Condition(() =>
+                {
+                    var result = Vector3.Distance(transform.position, _moveTo) <= 1;
+                    if (result)
+                    {
+                        _patrolIndex = (_patrolIndex + 1) % PatrolCheckPoints.Length;
+                        _moveTo = PatrolCheckPoints[_patrolIndex];
+                    }
+                    return result;
+                }, "decide")
+
+        ;
+    }
+
+    //private float _forwardAngle = 0;
+
+    private Vector3 lastPos=Vector3.zero;
+    // Update is called once per frame
+    void Update()
+    {
+        sm.Update();
+
+        if (lastPos != Vector3.zero)
+        {
+            var distance = Vector3.Distance(lastPos, transform.position);
+            //Debug.Log(distance);
+
+            var _forwardAngle = Mathf.Min(distance*300, 30);
+
+            var newRot = Quaternion.AngleAxis(_forwardAngle, Vector3.right);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, newRot, 2);
+        }
+        lastPos = transform.position;
+        //if (subsm.Current.Name == "moveTo")
+        //{
+        //    Debug.Log("moveTO " + _forwardAngle);
+        //    if (_forwardAngle <= 20)
+        //    {
+        //        _forwardAngle = Mathf.Min(_forwardAngle + 10 * Time.deltaTime, 20);
+
+        //        var newRot = Quaternion.AngleAxis(_forwardAngle, Vector3.right);
+        //        transform.rotation = Quaternion.RotateTowards(transform.rotation, newRot, 2);
+        //    }
+        //}
+        //else
+        //{
+        //    if (_forwardAngle > 0)
+        //    {
+        //        _forwardAngle = Mathf.Max(_forwardAngle - 10*Time.deltaTime, 0);
+
+        //        var newRot = Quaternion.AngleAxis(_forwardAngle, Vector3.right);
+        //        transform.rotation = Quaternion.RotateTowards(transform.rotation, newRot, 2);
+        //    }
+        //}
+
+    }
+
+    void OnDrawGizmos()
+    {
+        if (_moveTo != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(_moveTo, 1);
+        }
+    }
+
+}
---
+++ StateMachine.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StateMachine
+{
+    private Dictionary<string, StateModel> States { get; set; }
+    public StateModel Current { get; private set; }
+    private StateModel _old;
+    public string Name { get; private set; }
+    public bool Verbose = false;
+
+    public StateMachine(string name)
+    {
+        States = new Dictionary<string, StateModel>();
+        _old = Current = null;
+        Name = name;
+    }
+
+    public void Reset(StateModel stateModel)
+    {
+        _old = null;
+        Current = stateModel;
+    }
+
+    public void Reset(string stateName)
+    {
+        if (States.ContainsKey(stateName))
+            Current = States[stateName];
+        else
+            throw new Exception("StateModel name not found");
+    }
+
+    public StateModel State(string stateName)
+    {
+        var result = new StateModel(this)
+        {
+            Name = stateName
+        };
+
+        States.Add(stateName, result);
+
+        return result;
+    }
+
+    public void Update()
+    {
+        if (Current == null)
+            return;
+
+        if (Current.Id != (_old != null ? _old.Id : Guid.Empty))
+        {
+#if DEBUG
+//            if (Verbose)
+//            {
+//                if (_old == null)
+//                    Debug.Log(Time.time + "s " + Name + ": Initial StateModel=" + Current.Name);
+//                else
+//                    Debug.Log(Time.time + "s " + Name + ": " + _old.Name + " -> " + Current.Name);
+//            }
+#endif
+
+            Current.ExecEntry();
+            _old = Current;
+        }
+
+        Current.ExecState();
+
+        StateModel next;
+        var trigger = Current.Update(out next);
+
+        if (trigger)
+        {
+            Current.ExecExit();
+            if (Current == next)
+                Current.ExecEntry();
+            else
+                Current = next;
+        }
+    }
+
+    private StateModel GetState(string nextState)
+    {
+#if DEBUG
+        if (States.ContainsKey(nextState))
+            return States[nextState];
+
+        throw new Exception(Name + " does not have state \"" + nextState + "\"");
+#else
+        return States[nextState];
+#endif
+    }
+
+    public class StateModel
+    {
+        public Guid Id { get; private set; }
+        public StateMachine Parent { get; private set; }
+        public string Name { get; set; }
+        private List<Func<bool>> Conditions { get; set; }
+        private List<string> NextStates { get; set; }
+        private List<Action> OnEntryEvents { get; set; }
+        private List<Action> OnStateEvents { get; set; }
+        private List<Action> OnExitEvents { get; set; }
+
+        public StateModel(StateMachine parent)
+        {
+            Id = Guid.NewGuid();
+            Parent = parent;
+            Conditions = new List<Func<bool>>();
+            NextStates = new List<string>();
+
+            OnEntryEvents = new List<Action>();
+            OnStateEvents = new List<Action>();
+            OnExitEvents = new List<Action>();
+        }
+
+        public StateModel Condition(Func<bool> condition, string nextState)
+        {
+            Conditions.Add(condition);
+            NextStates.Add(nextState);
+
+            return this;
+        }
+
+        public bool Update(out StateModel state)
+        {
+            for (var index = 0; index < Conditions.Count; index++)
+            {
+                if (Conditions[index]())
+                {
+                    state = Parent.GetState(NextStates[index]);
+                    return true;
+                }
+            }
+
+            state = this;
+            return false;
+        }
+
+        public StateModel State(string stateName)
+        {
+            return Parent.State(stateName);
+        }
+
+        public StateModel OnEntry(Action onEntry)
+        {
+            OnEntryEvents.Add(onEntry);
+            return this;
+        }
+
+        public StateModel OnState(Action onState)
+        {
+            OnStateEvents.Add(onState);
+            return this;
+        }
+
+        public StateModel OnExit(Action onExit)
+        {
+            OnExitEvents.Add(onExit);
+            return this;
+        }
+
+        public StateModel Reset()
+        {
+            Parent.Reset(this);
+            return this;
+        }
+
+        public void ExecEntry()
+        {
+#if DEBUG
+            if (Parent.Verbose && OnEntryEvents.Count > 0)
+                Debug.Log(Time.time + "s " + Parent.Name + ": " + Name + ":OnEntry");
+#endif
+
+            Exec(OnEntryEvents);
+        }
+
+        public void ExecState()
+        {
+            Exec(OnStateEvents);
+        }
+
+        public void ExecExit()
+        {
+#if DEBUG
+            if (Parent.Verbose && OnExitEvents.Count > 0)
+                Debug.Log(Time.time + "s " + Parent.Name + ": " + Name + ":OnExit");
+#endif
+
+            Exec(OnExitEvents);
+        }
+
+        private void Exec(List<Action> actions)
+        {
+            actions.ForEach(action => action.Invoke());
+        }
+
+    }
+
+}
